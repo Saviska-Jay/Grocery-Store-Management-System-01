702266aac95e24eb2f42180dffa10e0deaead5dd
Saviska jayawickrama <SaviMJ@outlook.com>
2024-05-16 21:58:35 +0530

    Connection String was moved to the App.config file.

--- CashierLoginForm.cs
+++ CashierLoginForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Grocery_Store_Management_System_01
 {
@@ -18,7 +19,6 @@ namespace Grocery_Store_Management_System_01
             InitializeComponent();
         }
 
-        SqlConnection Con = new SqlConnection(@"Data Source=SAVISKAMJ\SQLEXPRESS;Initial Catalog=GroceryStoreManagementSystem01DB;Integrated Security=True;");
 
         private void label1_Click(object sender, EventArgs e)
         {
@@ -33,35 +33,51 @@ namespace Grocery_Store_Management_System_01
         //Cashier Login Button
         private void LoginButton_Click(object sender, EventArgs e)
         {
-            Con.Open();
-            String CashierLoginQ = "Select Username, Password From CashierLoginInfo Where Username= @Username And Password= @Password ";
-            SqlCommand cmd = new SqlCommand(CashierLoginQ, Con);
-
-            //SQL Parameters 
-            cmd.Parameters.AddWithValue("@Username", UsernameText.Text);
-            cmd.Parameters.AddWithValue("@Password", PasswordText.Text);
-
-            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            adapter.Fill(dt);
-            if (dt.Rows.Count > 0)
+            using (SqlConnection Con = new SqlConnection(ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString))
             {
+                try
+                { 
 
-                MessageBox.Show("Cashier Login Successful!", "Login Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                this.Hide();
-                CashierMainMenuForm CashierMainMenuForm = new CashierMainMenuForm();
-                CashierMainMenuForm.ShowDialog();
+                Con.Open();
+                String CashierLoginQ = "Select Username, Password From CashierLoginInfo Where Username= @Username And Password= @Password ";
+                SqlCommand cmd = new SqlCommand(CashierLoginQ, Con);
 
+                //SQL Parameters 
+                cmd.Parameters.AddWithValue("@Username", UsernameText.Text);
+                cmd.Parameters.AddWithValue("@Password", PasswordText.Text);
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                    {
+
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+                        if (dt.Rows.Count > 0)
+                        {
+
+                            MessageBox.Show("Cashier Login Successful!", "Login Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            this.Hide();
+                            CashierMainMenuForm CashierMainMenuForm = new CashierMainMenuForm();
+                            CashierMainMenuForm.ShowDialog();
+
+                        }
+                        else
+                        {
+
+                            MessageBox.Show("Invalied Cashier Username or Password. Please Try Again.", "Invalied Login!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            UsernameText.Clear();
+                            PasswordText.Clear();
+
+                        }
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Sorry. There is an Error." +ex ,"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+                }
             }
-            else
-            {
-
-                MessageBox.Show("Invalied Cashier Username or Password. Please Try Again.", "Invalied Login!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                UsernameText.Clear();
-                PasswordText.Clear();
-            
-            }
-            Con.Close();
         }
 
         //Cashier Cancel Button
